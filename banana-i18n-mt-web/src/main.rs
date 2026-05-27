82b4f230d010ba9a2cc28cf734bb1016a01120be
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2026-05-27 17:04:10 +0530

    make the post running on 0.0.0.0:8000
    for toolforge.

--- banana-i18n-mt-web/src/main.rs
+++ banana-i18n-mt-web/src/main.rs
@@ -127,8 +127,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .layer(CorsLayer::permissive())
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
-    info!("🚀 Server running at http://127.0.0.1:3000");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await?;
+    info!("🚀 Server running at http://0.0.0.0:8000");
 
     axum::serve(listener, app).await?;
 
